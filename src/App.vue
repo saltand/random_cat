01671b3700b4dc65c60148c8446885a748716e19
saltand <u6a69a9@outlook.com>
2023-10-27 16:48:43 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div style="display: flex; align-items: center; justify-content: center">
+    <img src="https://thecatapi.com/api/images/get?format=src&type=gif" class="image" v-if="isShow" @click="onClick" />
+    <div v-else>loading...</div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const isShow = ref(true)
+
+function onClick() {
+  isShow.value = false
+  setTimeout(() => {
+    isShow.value = true
+  }, 500)
+}
+</script>
+
+<style scoped>
+.image {
+  cursor: pointer;
+}
+</style>
