d75c179471f6cd8321adfc434638d13d9409a860
saltand <u6a69a9@outlook.com>
2023-10-30 11:20:27 +0800

    feat: http 弱请求

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <div style="display: flex; align-items: center; justify-content: center">
-    <img src="https://thecatapi.com/api/images/get?format=src&type=gif" class="image" v-if="isShow" @click="onClick" style="cursor: pointer" />
-    <div v-else>loading...</div>
+    <img :src="src" class="image" @click="onClick" style="cursor: pointer" @load="onload" v-show="!loading" />
+    <div v-show="loading">loading...</div>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-const isShow = ref(true)
+
+const loading = ref(false)
+const src = ref('https://thecatapi.com/api/images/get?format=src&type=gif')
 
 function onClick() {
-  isShow.value = false
-  setTimeout(() => {
-    isShow.value = true
-  }, 1000)
+  loading.value = true
+  src.value = 'https://thecatapi.com/api/images/get?format=src&type=gif' + '&time=' + Date.now()
+}
+
+function onload() {
+  loading.value = false
 }
 </script>
